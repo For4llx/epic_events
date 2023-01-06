4bbe427bd103e949f363f0f5655276175a6b9ed9
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2023-01-06 10:07:52 +0100

    adding models

--- epic_events/events/models.py
+++ epic_events/events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from authentification.models import User
 
 
 TYPES = [
@@ -13,37 +14,56 @@ PERMISSIONS = [
 ]
 
 STATUS = [
-    ('pending', 'Pending')
+    ('pending', 'Pending'),
     ('in progress', 'In progress'),
     ('done', 'Done'),
 ]
 
-class Client(models.Model):
-    client_id = models.BigAutoField(primary_key=True)
-    client_user = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
-    phone = models.CharField(_(""), max_length=50)
-    mobile = models.CharField(_(""), max_length=50)
-    company_name = models.CharField(_(""), max_length=50)
-    sales_contact = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
-    type = models.CharField(choices=TYPES, default='', max_length=50)
-
 
 class Staff(models.Model):
     staff_id =  models.BigAutoField(primary_key=True)
-    staff_user = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
+    staff_user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        blank=True)
     permission = models.CharField(choices=PERMISSIONS, default='', max_length=50)
+
+
+class Client(models.Model):
+    client_id = models.BigAutoField(primary_key=True)
+    client_user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        blank=True)
+    phone = models.CharField(max_length=50)
+    mobile = models.CharField(max_length=50)
+    company_name = models.CharField(max_length=50)
+    sales_contact = models.ForeignKey(
+        to=Staff,
+        on_delete=models.CASCADE,
+        blank=True)
+    type = models.CharField(choices=TYPES, default='', max_length=50)
+
+
+class Event(models.Model):
+    event_id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    status = models.CharField(choices=STATUS, default='', max_length=50)
+    support = models.ForeignKey(
+        to=Staff,
+        on_delete=models.CASCADE,
+        blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
 
 
 class Contract(models.Model):
     contract_id = models.BigAutoField(primary_key=True)
-    client_id = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
-    event_id = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
-    date_created = models.DateTimeField(auto_now_add=True)
-
-class Event(models.Model):
-    event_id = models.BigAutoField(primary_key=True)
-    contract_id = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
-    name = models.CharField(_(""), max_length=50)
-    status = models.CharField(choices=STATUS, default='', max_length=50)
-    event_staff = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
+    client_id = models.ForeignKey(
+        to=Client,
+        on_delete=models.CASCADE,
+        blank=True)
+    event_id = models.ForeignKey(
+        to=Event,
+        on_delete=models.CASCADE,
+        blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
